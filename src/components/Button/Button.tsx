3e5cbaaa79d0db19664d95a9ae734b8a6e7aa8fb
Zeeshan <zeeshan@guiker.com>
2020-06-20 20:10:18 +0530

    add animation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
+import { animated, useSpring } from 'react-spring';
 import { CustomTheme } from '../../theme';
 
 const useStyles = createUseStyles((theme: CustomTheme) => ({
@@ -21,15 +22,30 @@ const useStyles = createUseStyles((theme: CustomTheme) => ({
   },
 }));
 
+const calc: (x: number, y: number) => number[] = (x: number, y: number) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans: (x: number, y: number, s: number) => string | undefined = (x: number, y: number, s: number) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 interface ButtonProps {}
 
 const Button: React.FC<ButtonProps> = ({ children }) => {
   const classes: Record<'mainContainer', string> = useStyles();
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }));
 
   return (
-    <button onClick={() => window.open('mailto:sk.zeeshan1992@gmail.com', '_blank')} className={classes.mainContainer}>
+    <animated.button
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: props.xys.interpolate(trans as any) }}
+      onClick={() => window.open('mailto:sk.zeeshan1992@gmail.com', '_blank')}
+      className={classes.mainContainer}
+    >
       {children}
-    </button>
+    </animated.button>
   );
 };
 
