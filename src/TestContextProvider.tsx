95187a51f01e18ab6d7155cb6cdaca3b6ab17896
sk-izsk <sk.zeeshan1992@gmail.com>
2021-01-10 20:02:36 -0500

    add updated pkg

--- src/TestContextProvider.tsx
+++ src/TestContextProvider.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from 'react-jss';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { theme } from './theme';
 
+const queryClient = new QueryClient();
+
 const TestContextProvider: React.FC = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
 };
 
 export { TestContextProvider };
